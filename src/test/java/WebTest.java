c9d369ce11afdbbfec61cc98c54301f5d75e4d49
netol <figlovskayad@gmail.com>
2023-07-11 19:36:25 +0300

    3

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebTest {
+    private WebDriver driver;
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    public void beforeEach(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        driver.quit();
+        driver=null;
+    }
+    @Test
+    void shouldTest(){
+
+    }
+}
